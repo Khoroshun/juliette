93656ba1b92dfb19994b77647d2e48eb5dc8f33e
Andrii Khoroshun <a.s.khoroshun@gmail.com>
2019-05-22 13:56:03 +0300

    send sms

--- models/clients.go
+++ models/clients.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	u "github.com/khoroshun/juliette/utils"
-	"io/ioutil"
-	"net/http"
-	"strings"
+	sms "github.com/wildsurfer/turbosms-go"
+
+	"log"
 )
 
 type Client struct {
@@ -14,6 +14,7 @@ type Client struct {
 	Phone string `json:"phone"`
 	Name string `json:"name"`
 }
+
 
 /*
  This struct function validate the required parameters sent through the http request body
@@ -34,6 +35,7 @@ func (Client *Client) Validate() (map[string] interface{}, bool) {
 	return u.Message(true, "success"), true
 }
 
+
 func (Client *Client) Create() map[string] interface{} {
 
 	if resp, ok := Client.Validate(); !ok {
@@ -46,41 +48,14 @@ func (Client *Client) Create() map[string] interface{} {
 	resp["Client"] = Client
 
 
-	url := "http://turbosms.in.ua/api/soap.html"
+	c := sms.NewClient("JulietteBrand", "0997740160jb")
+	r, err := c.SendSMS("Juliette", Client.Phone, "Программа лояльности JULIETTE - переходи по ссылке и будь в курсе накопленных бонусов! https://juliette-sun.com.ua/check_bonus.php", "")
 
-	payload := strings.NewReader("<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:tur=\"http://turbosms.in.ua/api/Turbo\">\n   <soapenv:Header/>\n   <soapenv:Body>\n      <tur:Auth>\n         <!--Optional:-->\n         <tur:login>JulietteBrand</tur:login>\n         <!--Optional:-->\n         <tur:password>0997740160jb</tur:password>\n      </tur:Auth>\n   </soapenv:Body>\n</soapenv:Envelope>")
-
-	req, _ := http.NewRequest("POST", url, payload)
-
-	req.Header.Add("Content-Type", "application/xml")
-	req.Header.Add("cache-control", "no-cache")
-	req.Header.Add("Postman-Token", "510cac2e-3980-4c9a-9a60-1c9dfe2afe53")
-
-	res, _ := http.DefaultClient.Do(req)
-
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-
-	fmt.Println(res)
-	fmt.Println(string(body))
-
-
-	payload2 := strings.NewReader("<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:tur=\"http://turbosms.in.ua/api/Turbo\">\n   <soapenv:Header/>\n   <soapenv:Body>\n      <tur:SendSMS>\n         <!--Optional:-->\n         <tur:sender>Juliette</tur:sender>\n         <!--Optional:-->\n         <tur:destination>380967154107</tur:destination>\n         <!--Optional:-->\n         <tur:text>text text</tur:text>\n         <!--Optional:-->\n         <tur:wappush>?</tur:wappush>\n      </tur:SendSMS>\n   </soapenv:Body>\n</soapenv:Envelope>")
-
-	req2, _ := http.NewRequest("POST", url, payload2)
-
-	req.Header.Add("Content-Type", "application/xml")
-	req.Header.Add("cache-control", "no-cache")
-	req.Header.Add("Postman-Token", "5b8209f8-0bd9-48e7-80bd-c8792feb280a")
-
-	res2, _ := http.DefaultClient.Do(req2)
-
-	defer res.Body.Close()
-	body2, _ := ioutil.ReadAll(res.Body)
-
-	fmt.Println(res2)
-	fmt.Println(string(body2))
-
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("Result: %v\n", r.SendSMSResult)
+	}
 
 
 	return resp
