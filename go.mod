f8c52697d655961693763e09eec5a9cb40ca6d75
Khoroshun Andrii <a.s.khoroshun@gmail.com>
2020-01-31 17:14:33 +0200

    final fix before change server

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/khoroshun/juliette
+
+go 1.13
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.1-0.20180921172315-3af4c746e1c2+incompatible
+	github.com/gorilla/mux v1.7.1
+	github.com/jinzhu/gorm v1.9.3-0.20190310121721-8b07437717e7
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a
+	github.com/joho/godotenv v1.3.1-0.20190204044109-5c0e6c6ab1a0
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/lib/pq v1.0.1-0.20190314173650-7aad666537ab
+	github.com/robfig/cron v1.2.0
+	github.com/robfig/cron/v3 v3.0.0 // indirect
+	github.com/tools/godep v0.0.0-20180126220526-ce0bfadeb516 // indirect
+	github.com/wildsurfer/turbosms-go v0.0.2
+	golang.org/x/crypto v0.0.0-20190320223903-b7391e95e576
+	golang.org/x/tools v0.0.0-20191130070609-6e064ea0cf2d // indirect
+)
--- models/base.go
+++ models/base.go
@@ -1,18 +1,18 @@
 package models
 
 import (
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/jinzhu/gorm"
-	"os"
-	"github.com/joho/godotenv"
 	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
+	"os"
 )
 
 var db *gorm.DB //database
 
 func init() {
 
-	e := godotenv.Load() //Load .env file
+	e := godotenv.Load("/home/andrii/go/src/github.com/khoroshun/juliette/.env") //Load .env file
 	if e != nil {
 		fmt.Print(e)
 	}
@@ -24,6 +24,8 @@ func init() {
 
 
 	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Build connection string
+
+	dbUri  = "postgres://rbhpkwwiiyxnqv:473d42b05449fbe855d4c47054635789df479e2feb4aafe9bb736e1fb441a118@ec2-54-225-129-101.compute-1.amazonaws.com:5432/dcbpq4tkr0pn6o"
 
 	conn, err := gorm.Open("postgres", dbUri)
 	if err != nil {
